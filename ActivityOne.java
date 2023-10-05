ee6e727dd92b2d3c5deece3cd337f3d38bf5d292
Kinka Sugimoto <143375395+KinkaSug@users.noreply.github.com>
2023-10-05 21:02:57 +0800

    Add files via upload

---
+++ ActivityOne.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package activityone;
+
+/**
+ *
+ * @author Kinka Sugimoto
+ */
+public class ActivityOne {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("HELLO!!!!");
+        System.out.println("Testing if this will update file and work!");
+        System.out.println("Test");
+      
+    }
+    
+}
